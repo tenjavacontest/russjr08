36cda233397141aeda264882e0ad156ed2069f63
Russell Richardson <Russell@KronosAD.com>
2013-12-03 23:59:59 -0600

    Initial Commit. Skeleton code / maven initialization / plugin.yml setup. Added TODOs

---
+++ src/main/java/com/kronosad/projects/minecraft/tenjava/contest_1/Main.java
@@ -0,0 +1,35 @@
+/**
+ * This plugin is designed for the Ten.java plugin development contest. (Contest #1)
+ * Main plugin class file.
+ */
+package main.java.com.kronosad.projects.minecraft.tenjava.contest_1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * User: russjr08
+ * Date: 12/3/13
+ * Time: 11:50 PM
+ */
+public class Main extends JavaPlugin{
+
+
+    /**
+     * Called when the server enables this plugin. Listeners, CommandExecutors, Recipe Additions, etc. Are added/called
+     * in this method.
+     */
+    @Override
+    public void onEnable(){} //TODO: Actually do stuff here.
+
+    /**
+     * Called when the server disables this plugin. References to objects should be dereferences / nullified during this
+     * method call.
+     */
+    @Override
+    public void onDisable(){} //TODO: Dereference objects here.
+
+    //TODO: Override onCommand(args...) here if commands are needed.
+
+
+
+}
